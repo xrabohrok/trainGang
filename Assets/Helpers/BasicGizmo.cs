a05394db912b8f01ba1e6f7bcd5b207576217c8e
westin <westin007@gmail.com>
2015-01-24 12:18:08 -0600

    Added limiter to camera movement - Mr.B

---
+++ Assets/Helpers/BasicGizmo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicGizmo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireCube(transform.position, new Vector3(1,1,1));
+    }
+}
--- Assets/Helpers/CameraMove.cs
+++ Assets/Helpers/CameraMove.cs
@@ -7,8 +7,8 @@ public class CameraMove : MonoBehaviour {
     public float scrollRate = -5.0f;
     public float borderEdge = 30;
 
-    //public Transform LeftBound;
-    //public Transform RightBound;
+    public Transform LeftBound;
+    public Transform RightBound;
 	// Use this for initialization
 	void Start () {
 	
@@ -20,23 +20,29 @@ public class CameraMove : MonoBehaviour {
         //right edge
         if(Input.mousePosition.x > Screen.width - borderEdge )
         {
-            var percent = ((Input.mousePosition.x - (Screen.width - borderEdge))/ borderEdge);
-            if (Input.mousePosition.x > Screen.width)
-                percent = 1;
-            var newPos =new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + scrollRate * percent * Time.deltaTime);
+            if (RightBound == null || (RightBound != null && this.transform.position.z < RightBound.transform.position.z))
+            {
+                var percent = ((Input.mousePosition.x - (Screen.width - borderEdge)) / borderEdge);
+                if (Input.mousePosition.x > Screen.width)
+                    percent = 1;
+                var newPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + scrollRate * percent * Time.deltaTime);
 
-            this.transform.position = newPos;
+                this.transform.position = newPos;
+            }
         }
 
         //left edge
         if (Input.mousePosition.x <  borderEdge )
         {
-            var percent = ((Input.mousePosition.x) / borderEdge);
-            if (Input.mousePosition.x <= 0)
-                percent = 1;
-            var newPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + -scrollRate * percent * Time.deltaTime);
-            
-            this.transform.position = newPos;
+            if (LeftBound == null || (LeftBound != null && this.transform.position.z > LeftBound.transform.position.z))
+            {
+                var percent = ((Input.mousePosition.x) / borderEdge);
+                if (Input.mousePosition.x <= 0)
+                    percent = 1;
+                var newPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + -scrollRate * percent * Time.deltaTime);
+
+                this.transform.position = newPos;
+            }
         }
 	}
 }
