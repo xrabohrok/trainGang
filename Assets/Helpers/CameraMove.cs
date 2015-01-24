5cd1a9e24484305db88b6bfb76f219600114c027
westin <westin007@gmail.com>
2015-01-24 11:56:15 -0600

    Added left right scrolling - Mr.B

---
+++ Assets/Helpers/CameraMove.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CameraMove : MonoBehaviour {
+
+    public float scrollRate = -5.0f;
+    public float borderEdge = 30;
+
+    //public Transform LeftBound;
+    //public Transform RightBound;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        //right edge
+        if(Input.mousePosition.x > Screen.width - borderEdge )
+        {
+            var percent = ((Input.mousePosition.x - (Screen.width - borderEdge))/ borderEdge);
+            if (Input.mousePosition.x > Screen.width)
+                percent = 1;
+            var newPos =new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + scrollRate * percent * Time.deltaTime);
+
+            this.transform.position = newPos;
+        }
+
+        //left edge
+        if (Input.mousePosition.x <  borderEdge )
+        {
+            var percent = ((Input.mousePosition.x) / borderEdge);
+            if (Input.mousePosition.x <= 0)
+                percent = 1;
+            var newPos = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z + -scrollRate * percent * Time.deltaTime);
+            
+            this.transform.position = newPos;
+        }
+	}
+}
