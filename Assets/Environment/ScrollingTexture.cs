42130fe0b28d389b96cefbf517671b6e0495798d
westin <westin007@gmail.com>
2015-01-25 14:17:38 -0600

    scrolling terrain! - Mr.B

---
+++ Assets/Environment/ScrollingTexture.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class ScrollingTexture : MonoBehaviour {
+
+    public Vector2 speed;
+
+	// Use this for initialization
+    MeshRenderer renderer;
+	void Start () {
+        renderer = this.GetComponent<MeshRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        renderer.material.SetTextureOffset("_MainTex", renderer.material.GetTextureOffset("_MainTex") + Time.deltaTime * speed);
+	}
+}
